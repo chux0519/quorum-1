rumsystem/quorum

fix(wasm): enable rex protocol

pkg/wasm/quorum.go
@@ -76,6 +76,8 @@ func StartQuorum(qchan chan struct{}, password string, bootAddrs []string) (bool
 		return false, err
 	}
 
+	node.SetRumExchange(ctx, dbMgr)
+
 	nodectx.InitCtx(ctx, "default", node, dbMgr, "pubsub", "wasm-version")
 	nodectx.GetNodeCtx().Keystore = k
 	keys, err := quorumCrypto.SignKeytoPeerKeys(defaultKey)

chux0519/quorum-1

pkg/wasm/quorum.go
@@ -78,6 +78,7 @@ func StartQuorum(qchan chan struct{}, password string, bootAddrs []string) (bool
 	}
 
 	newchainstorage := chainstorage.NewChainStorage(dbMgr)
+	node.SetRumExchange(ctx, newchainstorage)
 	nodectx.InitCtx(ctx, "default", node, dbMgr, newchainstorage, "pubsub", "wasm-version")
 	nodectx.GetNodeCtx().Keystore = k
 	keys, err := quorumCrypto.SignKeytoPeerKeys(defaultKey)